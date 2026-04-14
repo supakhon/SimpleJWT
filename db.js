40b8adf4780a045b3c20d39a30d2724705063b1c
FF <supakhon2549@gmail.com>
2026-04-14 17:57:58 +0700

    Version 3

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite');
+const Datastore = require('nedb-promises');
 const path = require('path');
 
+const db = Datastore.create({ 
+    filename: path.join(__dirname, 'users.db'), 
+    autoload: true 
+});
+
 async function createDB() {
-    return open({
-        filename: path.join(__dirname, 'users.db'),
-        driver: sqlite3.Database,
-    });
+    return {
+        exec: async () => {},
+        run: async (query, params) => {
+            const newUser = { name: params[0], password: params[1] };
+            const doc = await db.insert(newUser);
+            return { lastID: doc._id };
+        },
+        get: async (query, params) => {
+            return await db.findOne({ name: params[0] });
+        }
+    };
 }
 
 module.exports = createDB;
